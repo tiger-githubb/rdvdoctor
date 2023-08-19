efd4aab9c0bb8730744176e2b6fdd6316033dc00
KARBOU Aristide <aristidetiger12@gmail.com>
2023-08-19 08:18:33 +0000

    add and bind data

--- src/pages/Home/HomeComponents/FeaturedHospitalsSection.tsx
+++ src/pages/Home/HomeComponents/FeaturedHospitalsSection.tsx
@@ -2,23 +2,7 @@ import React from 'react';
 import { Box, Container, Heading, SimpleGrid } from '@chakra-ui/react';
 import HospitalCard from '../../../components/HospitalCard';
 
-const featuredHospitals = [
-    {
-      id: 1,
-      name: 'Hôpital Saint-Louis',
-      address: 'Paris, France',
-      phoneNumber: '01 23 45 67 89',
-      imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    },
-    {
-      id: 2,
-      name: 'Hôpital Bellevue',
-      address: 'Marseille, France',
-      phoneNumber: '04 56 78 90 12',
-      imageUrl: 'https://picsum.photos/seed/picsum/200/300',
-    },
-
-  ];
+import { featuredHospitals } from '../../../services/hospitalData';
 
   
 const FeaturedHospitalsSection: React.FC = () => {
@@ -29,12 +13,9 @@ const FeaturedHospitalsSection: React.FC = () => {
                     Hôpitaux en vedette
                 </Heading>
                 <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
-                    
                         {featuredHospitals.map((hospital) => (
                             <HospitalCard  key={hospital.id} hospital={hospital} />
                         ))}
-                 
-
                 </SimpleGrid>
             </Container>
         </Box>
--- src/pages/Professionals/ProfessionalsPage.tsx
+++ src/pages/Professionals/ProfessionalsPage.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
-import { Flex, Box, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Flex, Box, Heading, SimpleGrid,Container } from '@chakra-ui/react';
 import ProfesionalCard from '../../components/ProfesionalCard';
+
+import { professionalsData } from '../../services/professionalData';
 
 const ProfessionalsPage = () => {
  
 
   return (
-    <Box p={1}>
+    <Container maxW="container.xl" mt={8}>
       <Flex justify="center" align="center" direction="column">
         <Heading as="h1" mb={4}>
           Liste des Professionnels
         </Heading>
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+        {professionalsData.map((professional) => (
+        <ProfesionalCard key={professional.id} professional={professional} />
+      ))}
         </SimpleGrid>
       </Flex>
-    </Box>
+    </Container>
   );
 };
 
