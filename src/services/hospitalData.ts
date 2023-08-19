02026e0ecd2fcc638051afab9f6e867659903de3
KARBOU Aristide <aristidetiger12@gmail.com>
2023-08-19 12:18:32 +0000

    data clean

--- src/services/hospitalData.ts
+++ src/services/hospitalData.ts
@@ -13,7 +13,7 @@ export const featuredHospitals = [
       phoneNumber: '04 56 78 90 12',
       imageUrl: 'https://picsum.photos/seed/picsum/200/300',
     },    {
-      id: 1,
+      id: 3,
       name: 'Hôpital Tata',
       address: 'lomé , togo',
       phoneNumber: '01 23 45 67 89',
