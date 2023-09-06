a8d09275b18c2dcac115618d2755658e0c6dd620
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-06 11:23:36 +0000

    routing with acces issue

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { auth } from '../services/firebase';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 
-const PrivateRoute: React.FC<{
-  element: React.ReactNode;
-}> = ({ element }) => {
+import { auth } from "../services/firebase";
+
+const PrivateRoute: React.FC<{ element: React.ReactNode }> = ({ element }) => {
   const user = auth.currentUser;
+  const token = localStorage.getItem("token");
 
-  if (!user) {
+  if (!token || !user) {
     return <Navigate to="/connexion" />;
   }
 
-  return <Route element={element} />;
+  return (
+    <Routes>
+      <Route element={element} />
+    </Routes>
+  );
 };
 
 export default PrivateRoute;
