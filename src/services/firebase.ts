04702258061d58e63d9593adcf16d79a21219dc5
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-08 07:47:54 +0000

    Firebase file update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,22 +2,27 @@ import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
 
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-KAQS3tzcm73ZHBlLUfI_t5R9a5acsQE",
   authDomain: "rdvdoctor-d36a1.firebaseapp.com",
-  databaseURL: "https://rdvdoctor-d36a1-default-rtdb.firebaseio.com",
   projectId: "rdvdoctor-d36a1",
   storageBucket: "rdvdoctor-d36a1.appspot.com",
   messagingSenderId: "608032171899",
-  appId: "1:608032171899:web:9edc8390302cce5c383fde"
+  appId: "1:608032171899:web:9edc8390302cce5c383fde",
+  databaseURL:"https://rdvdoctor-d36a1-default-rtdb.firebaseio.com/"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
+export const storage = getStorage();
+export const database = getDatabase();
 
 export default app;
 
