68fd1a19f7f6945e4b3ba082e5cf7750b1d45305
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-10 18:38:38 +0000

    get data

--- src/pages/Professionals/ProfessionalComponents/ProfessionalProfile.tsx
+++ src/pages/Professionals/ProfessionalComponents/ProfessionalProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Flex,
@@ -15,23 +15,31 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react';
-import { useParams } from 'react-router-dom';
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment';
-import 'moment/locale/fr';
-import { collection, doc, getDoc } from 'firebase/firestore'; 
-import { db } from '../../../services/firebase';
-import { ProfessionalData } from '../ProfessionalsPage';
+  ListItem,
+  UnorderedList,
+  List,
+  ListIcon,
+} from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import "moment/locale/fr";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { db } from "../../../services/firebase";
+import { ProfessionalData } from "../ProfessionalsPage";
 import { getDatabase, ref, onValue } from "firebase/database";
+import { FaCheckCircle } from "react-icons/fa";
 
-moment.locale('fr');
+moment.locale("fr");
 
 const ProfessionalProfile: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [professional, setProfessional] = useState<ProfessionalData | null>(null); 
+  const [professional, setProfessional] = useState<ProfessionalData | null>(
+    null
+  );
   const { uid } = useParams<{ uid: string }>();
+  const [availabilityData, setAvailabilityData] = useState<any | null>(null);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -44,15 +52,15 @@ const ProfessionalProfile: React.FC = () => {
   useEffect(() => {
     const fetchProfessionalData = async () => {
       try {
-        const professionalDocRef = doc(collection(db, 'users'), uid);
+        const professionalDocRef = doc(collection(db, "users"), uid);
         const professionalDocSnap = await getDoc(professionalDocRef);
         if (professionalDocSnap.exists()) {
           setProfessional(professionalDocSnap.data() as ProfessionalData);
         } else {
-          console.log('Professionnel non trouvé.');
+          console.log("Professionnel non trouvé.");
         }
       } catch (error) {
-        console.error('Une erreur est survenue :', error);
+        console.error("Une erreur est survenue :", error);
       }
     };
 
@@ -62,50 +70,81 @@ const ProfessionalProfile: React.FC = () => {
   }, [uid]);
 
   useEffect(() => {
-    const professionnelId = uid ;
+    const professionnelId = uid;
 
     const db = getDatabase();
-    const databaseRef =ref(db,`disponibilites_professionnels/${professionnelId}`);
+    const databaseRef = ref(
+      db,
+      `disponibilites_professionnels/${professionnelId}`
+    );
     onValue(databaseRef, (snapshot) => {
       if (snapshot.exists()) {
         const data = snapshot.val();
-        console.log(data);
-       console.log("donné dispo");
+        setAvailabilityData(data);
+        console.log("donné dispo", data);
       } else {
-        console.log('Aucune donnée trouvée dans la base de données Firebase.');
+        console.log("Aucune donnée trouvée dans la base de données Firebase.");
       }
     });
   }, []);
 
-
   return (
-    <Box p={4} >
-          {professional ? ( 
-
-      <><Flex direction="column" align="center" py={8}>
-          <Image src={professional.profile_image} alt={professional.displayName} boxSize="200px" borderRadius="full" />
-          <Heading mt={4} fontSize="xl">
-            {professional.displayName}
-          </Heading>
-          <Text color="gray.600">{professional.speciality}</Text>
-          <Text color={'gray.300'}>{professional.address}</Text>
-          <Button mt={4} colorScheme="pink" onClick={handleOpenModal}>
-            Prendre un rendez-vous
-          </Button>
-        </Flex><Divider my={6} /><Stack spacing={4}>
-
+    <Box p={4}>
+      {professional ? (
+        <>
+          <Flex direction="column" align="center" py={8}>
+            <Image
+              src={professional.profile_image}
+              alt={professional.displayName}
+              boxSize="200px"
+              borderRadius="full"
+            />
+            <Heading mt={4} fontSize="xl">
+              {professional.displayName}
+            </Heading>
+            <Text color="gray.600">{professional.speciality}</Text>
+            <Text color={"gray.300"}>{professional.address}</Text>
+            <Button mt={4} colorScheme="pink" onClick={handleOpenModal}>
+              Prendre un rendez-vous
+            </Button>
+          </Flex>
+          <Divider my={6} />
+          <Stack spacing={4}>
             <Text fontSize="xl" fontWeight="bold">
               Sélectionnez une date de rendez-vous
             </Text>
-
             <Text fontSize="xl" fontWeight="bold">
               Disponibilités
             </Text>
-     
-          </Stack><Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+ {availabilityData ? (
+        <UnorderedList mt={3} spacing={2} listStyleType="none">
+          {Object.entries(availabilityData).map(([day, slots]) => (
+            <ListItem key={day}>
+              <Stack direction="row" alignItems="center">
+                <ListIcon as={FaCheckCircle} color="green.500" />
+                <Text fontWeight="bold">{day}:</Text>
+              </Stack>
+              <List spacing={1} ml={6}>
+                {Object.entries(slots as { [key: string]: unknown }).map(([time, availability]) => (
+                  <ListItem key={time}>
+                    {time}: {(availability as string[]).join(" - ")}
+                  </ListItem>
+                ))}
+              </List>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      ) : (
+        <Text mt={3}>Aucune donnée de disponibilité trouvée.</Text>
+      )}
+
+          </Stack>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>Prendre un rendez-vous avec {professional.displayName}</ModalHeader>
+              <ModalHeader>
+                Prendre un rendez-vous avec {professional.displayName}
+              </ModalHeader>
               <ModalCloseButton />
               <ModalBody>
                 <Text>Choisissez une date de rendez-vous :</Text>
@@ -117,12 +156,13 @@ const ProfessionalProfile: React.FC = () => {
                 <Button colorScheme="blue">Prendre rendez-vous</Button>
               </ModalFooter>
             </ModalContent>
-          </Modal></>
-       ) : (
-        <div>Professionnel non trouvé.</div> 
+          </Modal>
+        </>
+      ) : (
+        <div>Professionnel non trouvé.</div>
       )}
     </Box>
   );
 };
- 
+
 export default ProfessionalProfile;
