04ab493b06f6239903311bba08c34a53831218cc
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-12 00:21:21 +0000

    add of sidebar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import HeaderDashboard from './DashboardComponents/HeaderDashboard';
-import AppointmentsList from './DashboardComponents/AppointmentsList';
-import MyCalendar from './DashboardComponents/Calendar';
+import SidebarWithHeader from './DashboardComponents/SidebarWithHeader';
 
 const Dashboard = () => {
-    // const appointments = [
-    //     {
-    //       id: 1,
-    //       patientName: 'John Doe',
-    //       date: '2023-08-25 10:00 AM',
-    //       reason: 'Checkup',
-    //       status: 'Confirmed',
-    //     },
-    //   ];
-   
 
   return (
     <Box p={4} >
-      <HeaderDashboard  />
-      <AppointmentsList/>
-      <MyCalendar/>
-
-      
+      <SidebarWithHeader />
     </Box>
   );
 };
