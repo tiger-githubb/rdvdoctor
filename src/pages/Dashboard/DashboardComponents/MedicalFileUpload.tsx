aa3b28d915c53360a9d2b4f8baa5c369de4dbd93
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-19 07:32:06 +0000

    cleaning

--- src/pages/Dashboard/DashboardComponents/MedicalFileUpload.tsx
+++ src/pages/Dashboard/DashboardComponents/MedicalFileUpload.tsx
@@ -1,7 +1,7 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useRef, useState } from "react";
 import { Flex, Button, FormLabel, FormControl } from "@chakra-ui/react";
-import { storage, db, auth } from "../../../services/firebase";
-import { updateDoc, doc, getFirestore } from "firebase/firestore";
+import { db, auth } from "../../../services/firebase";
+import { updateDoc, doc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
 
 interface MedicalFileFormProps {
@@ -11,17 +11,6 @@ interface MedicalFileFormProps {
 const MedicalFileUpload: FC<MedicalFileFormProps> = ({ userData }) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [file, setFile] = useState<File | null>(null);
-  const [fichier, setFichier] = useState(userData?.fichier);
-
-  useEffect(() => {
-    if (userData) {
-      setFichier(userData.fichier); 
-    }
-  }, [userData]);
-
-  const initialValues = {
-    fichier: userData?.fichier,
-  };
 
   const [formValues, setFormValues] = useState<{
     fichiers: string[];
@@ -30,8 +19,8 @@ const MedicalFileUpload: FC<MedicalFileFormProps> = ({ userData }) => {
   });
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      console.log(e.target.files[0].name);
+    if (e.target.files) {
+      console.log(e.target.files);
       setFile(e.target.files[0]);
     }
   };
@@ -52,6 +41,7 @@ const MedicalFileUpload: FC<MedicalFileFormProps> = ({ userData }) => {
         setFormValues((prevValues) => ({
           fichiers: [...(prevValues.fichiers), downloadURL],
         }));
+        
       } catch (error) {
         console.error("Erreur lors de l'envoi du fichier : ", error);
         alert("Une erreur s'est produite lors de l'envoi du fichier.");
@@ -61,11 +51,8 @@ const MedicalFileUpload: FC<MedicalFileFormProps> = ({ userData }) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    handleFileUpload()
 
-    setFormValues((prev) => ({
-      ...prev,
-      fichiers: prev.fichiers,
-    }));
     try {
       const user = auth.currentUser;
       if (user) {
@@ -105,7 +92,6 @@ const MedicalFileUpload: FC<MedicalFileFormProps> = ({ userData }) => {
 
       <Button
         type="submit"
-        onClick={handleFileUpload}
         colorScheme="blue"
         mt={4}
       >
