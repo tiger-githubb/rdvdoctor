08b0f180990e6e4637af2f48228a3eb1c7809797
KARBOU Aristide <aristidetiger12@gmail.com>
2023-09-22 13:06:07 +0000

    Skeleton for professionel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Box } from '@chakra-ui/react';
-import Hero from './HomeComponents/Hero';
-import Features from './HomeComponents/Features';
-import FeaturedProfessionalsSection from './HomeComponents/FeaturedProfessionalsSection';
-import FeaturedHospitalsSection from './HomeComponents/FeaturedHospitalsSection';
-import CallToAction from '../../components/CallToAction';
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
-import { db } from '../../services/firebase';
+import React, { useEffect, useState } from "react";
+import { Box, SkeletonCircle, SkeletonText, Spinner } from "@chakra-ui/react";
+import Hero from "./HomeComponents/Hero";
+import Features from "./HomeComponents/Features";
+import FeaturedProfessionalsSection from "./HomeComponents/FeaturedProfessionalsSection";
+import FeaturedHospitalsSection from "./HomeComponents/FeaturedHospitalsSection";
+import CallToAction from "../../components/CallToAction";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebase";
 
 interface HospitalData {
   createdByUserId: string;
-  created_at:string;
+  created_at: string;
   name: string;
   address: string;
   phone_number: string;
@@ -31,61 +31,75 @@ interface ProfessionalData {
 }
 
 const Home: React.FC = () => {
-  const [featuredHospitals, setFeaturedHospitals] = useState<HospitalData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [featuredHospitals, setFeaturedHospitals] = useState<HospitalData[]>(
+    []
+  );
   const [professionalsData, setProfessionalsData] = useState<
-  ProfessionalData[]
->([]);
+    ProfessionalData[]
+  >([]);
 
-const fetchProfessionalData = async () => {
-  try {
-    const professionalCollectionRef = collection(db, "users");
-    const professionalsSnapshot = await getDocs(professionalCollectionRef);
+  const fetchProfessionalData = async () => {
+    try {
+      setIsLoading(true);
+      const professionalCollectionRef = collection(db, "users");
+      const professionalsSnapshot = await getDocs(professionalCollectionRef);
 
-    const professionalsDataArray: ProfessionalData[] = [];
-    let count = 0; 
-    professionalsSnapshot.forEach((doc) => {
-      const professionalData = doc.data() as ProfessionalData;
-      if (professionalData.role === 1 && count < 3) {
-        professionalsDataArray.push(professionalData);
-      }
-    });
-    setProfessionalsData(professionalsDataArray);
-  } catch (error) {
-    console.log("un probleme est survenu :", error);
-  }
-};
+      const professionalsDataArray: ProfessionalData[] = [];
+      let count = 0;
+      professionalsSnapshot.forEach((doc) => {
+        const professionalData = doc.data() as ProfessionalData;
+        if (professionalData.role === 1 && count < 3) {
+          professionalsDataArray.push(professionalData);
+        }
+      });
+      setProfessionalsData(professionalsDataArray);
+      setIsLoading(false);
+    } catch (error) {
+      console.log("un probleme est survenu :", error);
+    }
+  };
 
-const fetchFeaturedHospitals = async () => {
-  try {
-    const hospitalsCollectionRef = collection(db, 'hospitals');
-    const q = query(hospitalsCollectionRef, orderBy('created_at', 'desc'), limit(3));
+  const fetchFeaturedHospitals = async () => {
+    try {
+      const hospitalsCollectionRef = collection(db, "hospitals");
+      const q = query(
+        hospitalsCollectionRef,
+        orderBy("created_at", "desc"),
+        limit(3)
+      );
 
-    const hospitalsSnapshot = await getDocs(q);
+      const hospitalsSnapshot = await getDocs(q);
 
-    const hospitalsDataArray: HospitalData[] = [];
-    hospitalsSnapshot.forEach((doc) => {
-      const hospitalData = doc.data();
-      hospitalsDataArray.push(hospitalData as HospitalData);
-    });
+      const hospitalsDataArray: HospitalData[] = [];
+      hospitalsSnapshot.forEach((doc) => {
+        const hospitalData = doc.data();
+        hospitalsDataArray.push(hospitalData as HospitalData);
+      });
 
-    setFeaturedHospitals(hospitalsDataArray);
-  } catch (error) {
-    console.error('Une erreur est survenue :', error);
-  }
-};
+      setFeaturedHospitals(hospitalsDataArray);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Une erreur est survenue :", error);
+    }
+  };
 
-useEffect(() => {
-  fetchProfessionalData();
-  fetchFeaturedHospitals();
-}, []);
+  useEffect(() => {
+    fetchProfessionalData();
+    fetchFeaturedHospitals();
+  }, []);
 
   return (
-    <Box p={0} >
-      <Hero />
-      <Features />
-      <FeaturedProfessionalsSection professionalsData={professionalsData}/>
-      <FeaturedHospitalsSection featuredHospitals={featuredHospitals}/>
-      <CallToAction />
+    <Box p={0}>
+
+        <>
+          <Hero />
+          <Features />
+          <FeaturedProfessionalsSection professionalsData={professionalsData} isLoading={isLoading} />
+          <FeaturedHospitalsSection featuredHospitals={featuredHospitals} />
+          <CallToAction />
+        </>
+      
     </Box>
   );
 };
--- src/pages/Home/HomeComponents/FeaturedProfessionalsSection.tsx
+++ src/pages/Home/HomeComponents/FeaturedProfessionalsSection.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
-import { Box, Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, SimpleGrid, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import ProfesionalCard from "../../../components/ProfesionalCard";
 
 
 
 interface FeaturedProfessionalsSectionProps {
   professionalsData: any;
+  isLoading: boolean;
 }
 
-const FeaturedProfessionalsSection: FC<FeaturedProfessionalsSectionProps> = ({ professionalsData }) => {
+const FeaturedProfessionalsSection: FC<FeaturedProfessionalsSectionProps> = ({ professionalsData,isLoading }) => {
 
   return (
     <Box w="100%"  m={0} p={0} py={12} bg="gray.100">
@@ -16,6 +17,22 @@ const FeaturedProfessionalsSection: FC<FeaturedProfessionalsSectionProps> = ({ p
         <Heading as="h2" size="lg" mb={4}>
           Professionnels de santé en vedette
         </Heading>
+        {isLoading ? (
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+        <Box padding="6" boxShadow="lg" bg="white">
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+        </Box>
+        <Box padding="6" boxShadow="lg" bg="white">
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+        </Box>
+        <Box padding="6" boxShadow="lg" bg="white">
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+        </Box>
+        </SimpleGrid>
+      ) : (
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
           {professionalsData.map((professional:any) => (
             <ProfesionalCard
@@ -24,6 +41,7 @@ const FeaturedProfessionalsSection: FC<FeaturedProfessionalsSectionProps> = ({ p
             />
           ))}
         </SimpleGrid>
+        )}
       </Container>
     </Box>
   );
